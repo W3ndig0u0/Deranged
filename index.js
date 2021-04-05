8d13da2fb9e8e13e7b8b1e30449488b38e7cda9c
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-05 18:33:56 +0200

    :)

---
+++ index.js
@@ -0,0 +1,38 @@
+
+document.addEventListener('readystatechange', event => { 
+
+  if (event.target.readyState === "complete") {
+  // !Starta funktionen när sidan startar
+
+  write();
+  console.log("Start");
+}
+});
+
+var text = new Array("welcome back, we missed you,");
+
+var i = 0;
+var arrayLength = text[0].length;
+var position = 0;
+
+randomnumber = Math.floor(Math.random() * (200 - 500 + 1)) + 500;
+
+
+
+function write()
+{
+ var sContents =  ' ';
+ var destination = document.getElementById("title");
+ destination.innerHTML = sContents + text[i].substring(0, position) + ",,,";
+ if ( position++ == arrayLength ) {
+  position = 0;
+  i++;
+
+  if ( i != text.length ) {
+   arrayLength = text[i].length;
+  }
+} else {
+   setTimeout("write()", randomnumber);
+ }
+ console.log(randomnumber);
+}
