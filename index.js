fd43122323c592e3027f24402795fb0bc125d75a
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-05 21:02:11 +0200

    asdasd

--- index.js
+++ index.js
@@ -32,11 +32,3 @@ function write()
  }
  console.log(randomnumber);
 }
-
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-if (!isChrome){
-    $('#iframeAudio').remove()
-}
-else {
-    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-}
